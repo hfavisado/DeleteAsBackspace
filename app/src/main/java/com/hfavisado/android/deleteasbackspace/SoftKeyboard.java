cd5dd103502ad1f22e4b23c77680fa2e20dc5998
Hal Avisado <hal.avisado@rakuten.com>
2016-04-13 16:38:22 +0900

    Get app name and icons ready for publishing on Google Play

--- app/src/main/java/com/hfavisado/android/deleteasbackspace/SoftKeyboard.java
+++ app/src/main/java/com/hfavisado/android/deleteasbackspace/SoftKeyboard.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.softkeyboard;
+package com.hfavisado.android.deleteasbackspace;
 
 import android.app.Dialog;
 import android.inputmethodservice.InputMethodService;
